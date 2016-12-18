88f8a3974d799ec38a49ad66c56fa4056512b342
chaeplin <chaeplin@gmail.com>
2016-12-19 04:37:57 +0900

    b58decode

--- etc/addrgen-raw.py
+++ etc/addrgen-raw.py
@@ -23,22 +23,64 @@ long = int
 _bchr = lambda x: bytes([x])
 _bord = lambda x: x
 
-_base58_codestring = b'123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz'
-_base58_codestring_len = len (_base58_codestring)
+if str != bytes:
+    # Python 3.x
+    def ord(c):
+        return c
+    def chr(n):
+        return bytes( (n,) )
 
-def b58encode (x):
-    q = int.from_bytes (x, 'big')
-    result = bytearray ()
-    while q > 0:
-            q, r = divmod (q, _base58_codestring_len)
-            result.append (_base58_codestring[r])
-    for c in x:
-            if c == 0:
-                    result.append (_base58_codestring[0])
-            else:
-                    break
-    result.reverse ()
-    return bytes (result).decode("utf-8")
+__b58chars = '123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz'
+__b58base = len(__b58chars)
+b58chars = __b58chars
+
+def b58encode(v):
+    """ encode v, which is a string of bytes, to base58.
+    """
+    long_value = 0
+    for (i, c) in enumerate(v[::-1]):
+        long_value += (256**i) * ord(c)
+
+    result = ''
+    while long_value >= __b58base:
+        div, mod = divmod(long_value, __b58base)
+        result = __b58chars[mod] + result
+        long_value = div
+    result = __b58chars[long_value] + result
+
+    # Bitcoin does a little leading-zero-compression:
+    # leading 0-bytes in the input become leading-1s
+    nPad = 0
+    for c in v:
+        if c == '\0': nPad += 1
+        else: break
+
+    return (__b58chars[0]*nPad) + result
+
+def b58decode(v, length = None):
+    """ decode v into a string of len bytes
+    """
+    long_value = 0
+    for (i, c) in enumerate(v[::-1]):
+        long_value += __b58chars.find(c) * (__b58base**i)
+
+    result = bytes()
+    while long_value >= 256:
+        div, mod = divmod(long_value, 256)
+        result = chr(mod) + result
+        long_value = div
+    result = chr(long_value) + result
+
+    nPad = 0
+    for c in v:
+        if c == __b58chars[0]: nPad += 1
+        else: break
+
+    result = chr(0)*nPad + result
+    if length is not None and len(result) != length:
+        return None
+
+    return result
 
 def private_key_to_wif(string):
     prv = binascii.unhexlify(string)
