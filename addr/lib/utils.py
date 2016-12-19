d30a1c9f9d0bbfee71188582c1b92d8b06f93246
chaeplin <chaeplin@gmail.com>
2016-12-20 05:26:14 +0900

    to lib

---
+++ addr/lib/utils.py
@@ -0,0 +1,34 @@
+# utils.py
+
+# Elliptic curve parameters (secp256k1)
+
+P = 2**256 - 2**32 - 977
+N = 115792089237316195423570985008687907852837564279074904382605163141518161494337
+A = 0
+B = 7
+Gx = 55066263022277343669578718895168534326250603453777594175500187360389116729240
+Gy = 32670510020758816978083085130507043184471273380659243275938904335757337482424
+G = (Gx, Gy)
+
+#
+OP_DUP = 0x76
+OP_HASH160 = 0xa9
+OP_EQUALVERIFY = 0x88
+OP_CHECKSIG = 0xac
+OP_RETURN   = 0x6a
+
+#
+string_types = (str)
+string_or_bytes_types = (str, bytes)
+int_types = (int, float)
+
+code_strings = {
+    2: '01',
+    10: '0123456789',
+    16: '0123456789abcdef',
+    32: 'abcdefghijklmnopqrstuvwxyz234567',
+    58: '123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz',
+    256: ''.join([chr(x) for x in range(256)])
+}
+
+#
---
+++ addr/scripts.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+from lib.script import *
+
+def script_forma_5():                                      
+    script_hex = '76a914fd85adfcf0c5c6a3f671428a7bfa3944cb84030588ac'
+    print('yjRwwxf95GtmK41oEH9VDPdDCtV1Jczmip', script_to_addr(script_hex), len(binascii.unhexlify(script_hex)))
+
+def script_forma_1(): 
+    script_hex = '41047559d13c3f81b1fadbd8dd03e4b5a1c73b05e2b980e00d467aa9440b29c7de23664dde6428d75cafed22ae4f0d302e26c5c5a5dd4d3e1b796d7281bdc9430f35ac'
+    print('yb21342iADyqAotjwcn4imqjvAcdYhnzeH', script_to_addr(script_hex), len(binascii.unhexlify(script_hex)))
+
+def script_forma_2(): 
+    script_hex = '047559d13c3f81b1fadbd8dd03e4b5a1c73b05e2b980e00d467aa9440b29c7de23664dde6428d75cafed22ae4f0d302e26c5c5a5dd4d3e1b796d7281bdc9430f35ac'
+    print('yb21342iADyqAotjwcn4imqjvAcdYhnzeH', script_to_addr(script_hex), len(binascii.unhexlify(script_hex)))
+
+def script_forma_3():
+    script_hex = '76a914fd85adfcf0c5c6a3f671428a7bfa3944cb84030588acacaa'
+    print('yjRwwxf95GtmK41oEH9VDPdDCtV1Jczmip', script_to_addr(script_hex), len(binascii.unhexlify(script_hex)))
+
+def script_forma_4():                                       
+    script_hex = '76a90088ac'
+    print('unspendable', script_to_addr(script_hex), len(binascii.unhexlify(script_hex)))
+
+def script_p2p():
+    script_hex = '6a281adb1bf4cef81ede4a63ad5ca5943e5288fffc210d90a861a60a96658d7f90580000000000000000'
+    print('nulldata', script_to_addr(script_hex))
+
+def script_compressed():
+    script_hex = '2103717f7082f58395f02afb45b1ae871cae31293b33c64c8d9568d9cac09fa70c51ac'
+    print('yhF732jM8hA2e5svfBCa1heFbdHVNXdM8n', script_to_addr(script_hex), len(binascii.unhexlify(script_hex)))    
+
+# check
+
+script_forma_5()
+script_forma_1()
+script_forma_2()
+script_forma_3()
+script_forma_4()
+script_p2p()
+script_compressed()
+
